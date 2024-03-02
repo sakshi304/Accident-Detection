4fa56e303c5ee4d822fa764ec1b2b6d101cff66e
1-Arnav <90562006+1-Arnav@users.noreply.github.com>
2024-03-03 01:07:00 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from model1 import predictor
+from predictor import predict
 
 st.title(" Accident Detection using machine learning")
 
 uploaded_file = st.file_uploader("Upload your file here...", type=['png', 'jpeg', 'jpg'])
 
-if uploaded_file is not None:
-    st.image(uploaded_file)
-
 try:
-    st.write(predictor(uploaded_file))
+    st.write(predict(uploaded_file))
 except:
     st.warning("Please upload a file")
+
+if uploaded_file is not None:
+    st.image(uploaded_file)
