2c47f702689e5eb9f62c9da1f761a55b8d74e241
1-Arnav <90562006+1-Arnav@users.noreply.github.com>
2024-03-03 01:00:46 +0530

    Update and rename model1.py to predictor.py
    removed some stuff as well

--- predictor.py
+++ predictor.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 
 
-def predicter(img):
+def predictor(img):
     im=Image.open(img).resize((250,250))
     img_array = tf.keras.utils.img_to_array(im)
     img_batch = np.expand_dims(img_array, axis=0)
@@ -15,12 +14,6 @@ def predicter(img):
     interpreter.resize_tensor_input(input_details[0]['index'], (1, 250, 250,3))
     interpreter.resize_tensor_input(output_details[0]['index'], (1, 2))
     interpreter.allocate_tensors()
-    # input_details = interpreter.get_input_details()
-    # output_details = interpreter.get_output_details()
-    # print("Input Shape:", input_details[0]['shape'])
-    # print("Input Type:", input_details[0]['dtype'])
-    # print("Output Shape:", output_details[0]['shape'])
-    # print("Output Type:", output_details[0]['dtype'])
 
     interpreter.set_tensor(input_details[0]['index'], img_batch)
     interpreter.invoke()
@@ -30,5 +23,3 @@ def predicter(img):
         return("Accident Detected")
     else:
         return("No Accident")
-
-    # print(plt.imshow(im))
